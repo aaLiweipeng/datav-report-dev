9663b03f0300ec42c6719b50fe0f5a0011db2530
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2021-10-06 02:05:06 +0800

    preliminarily completed the TotalOrders Card

--- src/components/TotalOrders/index.vue
+++ src/components/TotalOrders/index.vue
@@ -1,5 +1,17 @@
 <template>
-    <CommonCard />
+    <CommonCard
+      title="累计订单量"
+      value="2,157,420">
+
+      <template>
+        <div id="total-orders-chart" :style="{ width: '100%', height:'100%'}" />
+      </template>
+
+      <template v-slot:footer>
+        <span>昨日订单量</span>
+        <span class="emphasis">2,000,000</span>
+      </template>
+    </CommonCard>
 </template>
 
 <script>
@@ -7,7 +19,41 @@
 import commonCardMixin from '../../mixins/commonCardMixins'
 
 export default {
-  mixins: [commonCardMixin]
+  mixins: [commonCardMixin],
+  mounted () {
+    const chartDom = document.getElementById('total-orders-chart')
+    const chart = this.$echarts.init(chartDom)
+    chart.setOption({
+      xAxis: {
+        type: 'category',
+        show: false,
+        boundaryGap: false // 将图左右舒展开【除去x轴 默认的左右间距】
+      },
+      yAxis: {
+        show: false
+      },
+      series: [{
+        type: 'line',
+        data: [620, 432, 222, 556, 798, 433, 256, 323, 526, 353, 896, 696, 525, 225, 668],
+        areaStyle: {
+          color: 'blue' // 面积填充
+        },
+        lineStyle: {
+          width: 0 // 连线宽度
+        },
+        itemStyle: {
+          opacity: 0 // 设置 连线上的点的 透明度
+        },
+        smooth: true // 连线平滑
+      }],
+      grid: {
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0
+      } // 优化布局展示，把图表的数据和UI 按照 合适的比例 fit到 wrapper-div的组件区域中
+    })
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-import ECharts from 'echarts'
+import * as echarts from 'echarts'
 import './style/index.scss'
 
 Vue.config.productionTip = false
-Vue.prototype.$echarts = ECharts
+Vue.prototype.$echarts = echarts
 
 new Vue({
   router,
